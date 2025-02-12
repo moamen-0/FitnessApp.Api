382b880b3dc2808ac51fed56da66f1e8fc1d70dc
moamen-0 <212103128@o6u.edu.eg>
2025-02-12 16:12:34 +0200

    generic repository

--- FitnessApp.Core/Entities/ApplicationUser.cs
+++ FitnessApp.Core/Entities/ApplicationUser.cs
@@ -6,6 +6,14 @@ namespace FitnessApp.Core.Entities
 {
 	public class ApplicationUser : IdentityUser
 	{
-		public string DisplayName { get; set; }
+		public string FullName { get; set; } = string.Empty;
+		public string Gender { get; set; } = string.Empty;
+		public DateTime DateOfBirth { get; set; }
+		public string? ProfileImage { get; set; }
+
+		 
+		public ICollection<WorkoutPlan> WorkoutPlans { get; set; } = new List<WorkoutPlan>();
+		public ICollection<DietPlan> DietPlans { get; set; } = new List<DietPlan>();
+		public ICollection<InBodyAnalysis> InBodyAnalyses { get; set; } = new List<InBodyAnalysis>();
 	}
 }
---
+++ FitnessApp.DAL/Repositories/GenericRepository.cs
@@ -0,0 +1,61 @@
+﻿using FitnessApp.Core.Interfaces;
+using FitnessApp.FitnessApp.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FitnessApp.DAL.Repositories
+{
+	public class GenericRepository<T> : IGenericRepository<T> where T : class
+	{
+		private readonly AppDbContext _context;
+		private readonly DbSet<T> _dbSet;
+
+		public GenericRepository(AppDbContext context)
+		{
+			_context = context;
+			_dbSet = context.Set<T>();
+		}
+
+		public Task AddAsync(T entity)
+		{
+			_dbSet.Add(entity);
+			return _context.SaveChangesAsync();
+
+
+		}
+
+		public async Task DeleteAsync(T entity)
+		{
+			_dbSet.Remove(entity);
+			await _context.SaveChangesAsync();
+
+		}
+
+		public async Task<IEnumerable<T>> GetAllAsync()
+		{
+			return await Task.FromResult(_dbSet.AsEnumerable());
+			}
+
+		public async Task<T?> GetByIdAsync(int id)
+		{
+			var entity = _dbSet.Find(id);
+			if (entity == null)
+			{
+				return await Task.FromResult<T?>(null);
+			}
+
+			return await _dbSet.FindAsync(id);
+
+		}
+
+		public async Task UpdateAsync(T entity)
+		{
+			_dbSet.Update(entity);
+			await _context.SaveChangesAsync();
+		}
+	}
+}
