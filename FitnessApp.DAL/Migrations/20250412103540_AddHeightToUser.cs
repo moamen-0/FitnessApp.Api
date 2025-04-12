e7d819eb59a044db1c3e9bbbe9ff895db7358f2f
moamen-0 <212103128@o6u.edu.eg>
2025-04-12 12:36:44 +0200

    Add Height to user table

---
+++ FitnessApp.DAL/Migrations/20250412103540_AddHeightToUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FitnessApp.DAL.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHeightToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "AspNetUsers");
+        }
+    }
+}
